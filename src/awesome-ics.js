2ff3058cd73bb28ddad19f9baf948281ccba6923
bandro <bandraszyk@gmail.com>
2015-08-15 20:12:45 +0200

    Removed old files

--- src/awesome-ics.js
+++
@@ -1,7 +0,0 @@
-//     awesomeIcs.js 1.0.0
-//     http://bandraszyk.github.io/awesome-ics/
-//     (c) 2015 Bandro
-//     Awesome Rating may be freely distributed under the MIT lice
-var Awesome = {};
-
-module.exports = Awesome;
